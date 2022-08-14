26903f40ac99e81965baa435ef216352167aec3d
sigoden <sigoden@gmail.com>
2022-08-14 23:52:41 +0800

    chore: add Debug for GlobRule

--- crates/jsona-util/src/util.rs
+++ crates/jsona-util/src/util.rs
@@ -4,6 +4,7 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use std::{
     borrow::Cow,
+    fmt::Debug,
     path::{Path, PathBuf},
 };
 use url::Url;
@@ -11,10 +12,21 @@ use url::Url;
 const FILE_PROTOCOL: &str = "file://";
 static RE_URI_PROTOCOL: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\w+://)").unwrap());
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct GlobRule {
+    raw_include: Vec<String>,
+    raw_exclude: Vec<String>,
     include: globset::GlobSet,
     exclude: globset::GlobSet,
+}
+
+impl Debug for GlobRule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("GlobRule")
+            .field("include", &self.raw_include)
+            .field("exclude", &self.raw_exclude)
+            .finish()
+    }
 }
 
 impl GlobRule {
@@ -23,18 +35,24 @@ impl GlobRule {
         exclude: impl IntoIterator<Item = impl AsRef<str>>,
     ) -> Result<Self, anyhow::Error> {
         let mut inc = GlobSetBuilder::new();
+        let mut raw_include = vec![];
         for glob in include {
             inc.add(Glob::new(glob.as_ref())?);
+            raw_include.push(glob.as_ref().to_string());
         }
 
         let mut exc = GlobSetBuilder::new();
+        let mut raw_exclude = vec![];
         for glob in exclude {
             exc.add(Glob::new(glob.as_ref())?);
+            raw_exclude.push(glob.as_ref().to_string());
         }
 
         Ok(Self {
             include: inc.build()?,
             exclude: exc.build()?,
+            raw_include,
+            raw_exclude,
         })
     }
 
