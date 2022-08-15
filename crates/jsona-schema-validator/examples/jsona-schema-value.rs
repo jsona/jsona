60a3dbf72337b544444b3e0c2a16411d537eada1
sigoden <sigoden@gmail.com>
2022-08-15 09:46:25 +0800

    chore(example): update jsona-schema-value

--- crates/jsona-schema-validator/examples/jsona-schema-value.rs
+++ crates/jsona-schema-validator/examples/jsona-schema-value.rs
@@ -11,20 +11,9 @@ fn main() {
     let jsona_content = std::fs::read_to_string(jsona_file_path).unwrap();
     let node: Node = jsona_content.parse().expect("invalid jsona doc");
     let schema_value = JSONASchemaValue::from_node(node).expect("invalid jsona schema value");
-    let result = match keys {
-        Some(keys) => {
-            let keys: Keys = keys.parse().unwrap();
-            if keys
-                .last()
-                .map(|v| v.is_annotation_key())
-                .unwrap_or_default()
-            {
-                serde_json::to_string_pretty(&schema_value.annotations.pointer(&keys)).unwrap()
-            } else {
-                serde_json::to_string_pretty(&schema_value.value.pointer(&keys)).unwrap()
-            }
-        }
-        None => serde_json::to_string_pretty(&schema_value).unwrap(),
-    };
-    println!("{}", result);
+	let keys: Keys = match keys {
+		Some(keys) => keys.parse().unwrap(),
+		None => Keys::default(),
+	};
+    println!("{}", serde_json::to_string_pretty(&schema_value.pointer(&keys)).unwrap());
 }
