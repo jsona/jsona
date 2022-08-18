8a03ef1541abf5392021268a974f08d98c8122c3
sigoden <sigoden@gmail.com>
2022-08-18 16:26:35 +0800

    feat: optimize list_schemas

--- crates/jsona-lsp/src/handlers/schema.rs
+++ crates/jsona-lsp/src/handlers/schema.rs
@@ -5,8 +5,11 @@ use crate::{
     },
     world::World,
 };
-use jsona_util::{environment::Environment, schema::associations::AssociationRule};
+use indexmap::IndexMap;
+use jsona_util::{environment::Environment, schema::associations::priority};
 use lsp_async_stub::{rpc::Error, Context, Params};
+use serde_json::Value;
+use url::Url;
 
 #[tracing::instrument(skip_all)]
 pub async fn list_schemas<E: Environment>(
@@ -20,15 +23,25 @@ pub async fn list_schemas<E: Environment>(
     let ws = workspaces.by_document(&document_uri);
 
     let associations = ws.schemas.associations().read();
+    let mut map: IndexMap<Url, Value> = associations
+        .iter()
+        .filter(|(_, s)| matches!(s.priority, priority::STORE))
+        .map(|(_, s)| (s.url.clone(), s.meta.clone()))
+        .collect();
+    for (_, s) in associations
+        .iter()
+        .filter(|(_, s)| s.priority != priority::STORE)
+    {
+        if map.contains_key(&s.url) {
+            continue;
+        }
+        map.insert(s.url.clone(), s.meta.clone());
+    }
 
     Ok(ListSchemasResponse {
-        schemas: associations
-            .iter()
-            .filter(|(rule, _)| !matches!(rule, AssociationRule::Url(..)))
-            .map(|(_, s)| SchemaInfo {
-                url: s.url.clone(),
-                meta: s.meta.clone(),
-            })
+        schemas: map
+            .into_iter()
+            .map(|(url, meta)| SchemaInfo { url, meta })
             .collect(),
     })
 }
